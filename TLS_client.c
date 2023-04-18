b5b96250c95b5e4f27e57739bdffc21230b0c411
Fred Frigo <54369199+FrigoF@users.noreply.github.com>
2023-04-18 13:56:26 -0500

    Update TLS_client.c

--- TLS_client.c
+++ TLS_client.c
@@ -7,7 +7,7 @@
 // See https://wiki.openssl.org/index.php/Simple_TLS_Server
 //
 //  To install OpenSSL see INSTALL at https://www.openssl.org/source/
-//  To compile: gcc -Wall -o TLS_client TLS_client.c -L/usr/lib -lssl -lcrypto
+//  To compile: gcc -Wall -o TLS_client TLS_client.c -lssl -lcrypto -L/usr/local/lib
 //
 #include <stdio.h>
 #include <unistd.h>
